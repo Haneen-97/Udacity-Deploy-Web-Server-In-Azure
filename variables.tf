642b5448c2a4c884cca087d2957e66af171ac0db
Haneen H.H <43758373+Haneen-97@users.noreply.github.com>
2021-01-18 15:44:31 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,35 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  default = "udacity-azure"
+}
+
+variable "environment"{
+  description = "The environment should be used for all resources in this example"
+  default = "test"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default = "uk south" 
+}
+
+variable "username"{
+  default = "username"
+}
+
+variable "password"{
+  default= "Password11@"
+}
+
+variable "server_names"{
+  type = list
+  default = ["uat","int"]
+}
+
+variable "packerImageId"{
+  default = "/subscriptions/XXXXXXXXXXXXXXX/resourceGroups/packer-rg/providers/Microsoft.Compute/images/udacityPackerImage"
+}
+
+variable "vm_count"{
+  default = "2"
 }
